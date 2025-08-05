d30e3f3f01040907bdad5decdad59fe9ee0be6a7
Mike Monaco <mamonaco1973@gmail.com>
2025-08-05 13:28:26 -0400

    fix destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
-MI_INSTANCE=$(az sql mi list \
-  --resource-group sqlserver-rg \
-  --query "[?starts_with(name, 'sqlmi')].name" \
-  --output tsv)
-
-if [ -n "$MI_INSTANCE" ]; then
-  echo "NOTE: Deleting Managed Instance: $MI_INSTANCE"
-  az sql mi delete \
-    --name "$MI_INSTANCE" \
-    --resource-group sqlserver-rg \
-    --yes
-else
-  echo "WARNING: No Managed Instance found starting with 'sqlmi'. Skipping deletion."
-fi
-
-
 #-------------------------------------------------------------------------------
 # STEP 1: Destroy sqlserver infrastructure (VNet, Subnet, NICs, NSGs, etc.)
 #-------------------------------------------------------------------------------
 cd 01-sqlserver                    # Go to base infra config
 terraform init                     # Initialize Terraform plugins/modules
+terraform destroy -target=azurerm_mssql_managed_instance.sql_mi -auto-approve 
 terraform destroy -auto-approve    # Destroy all foundational Azure resources
 cd ..                              # Return to root
 
