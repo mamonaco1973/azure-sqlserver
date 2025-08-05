7a15f2e4d8ec347bc7d898cc74f74bd45707b976
mamonaco1973 <mamonaco1973@gmail.com>
2025-08-05 16:22:44 +0000

    fix destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+MI_INSTANCE=$(az sql mi list \
+  --resource-group sqlserver-rg \
+  --query "[?starts_with(name, 'sqlmi')].name" \
+  --output tsv)
+
+if [ -n "$MI_INSTANCE" ]; then
+  echo "NOTE: Deleting Managed Instance: $MI_INSTANCE"
+  az sql mi delete \
+    --name "$MI_INSTANCE" \
+    --resource-group sqlserver-rg \
+    --yes
+else
+  echo "WARNING: No Managed Instance found starting with 'sqlmi'. Skipping deletion."
+fi
+
+
 #-------------------------------------------------------------------------------
 # STEP 1: Destroy sqlserver infrastructure (VNet, Subnet, NICs, NSGs, etc.)
 #-------------------------------------------------------------------------------
